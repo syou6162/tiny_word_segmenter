2147f1383204e9088885a17081448e9c5ec4a2ab
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-20 03:13:31 +0900

    負例を0ではなく-1に変更
    news20binaryの例がそうだったので。合わせて正規表現のやり方とテストも変更。

--- src/TinyWordSegmenter/util.clj
+++ src/TinyWordSegmenter/util.clj
@@ -2,11 +2,11 @@
   (:use [clojure.contrib.string :only (split)]))
 
 (defn parse-line [line]
-  (let [[y fv] (-> (re-seq #"([0|1])\s(.*)" line)
+  (let [[y fv] (-> (re-seq #"(-?1)\s(.*)" line)
 		   (first)
 		   (rest))]
     [(Integer/parseInt y) (->> fv
 			       (split #"\s")
 			       (map #(let [[xi cnt] (split #":" %)]
-				       [(Integer/parseInt xi) (Integer/parseInt cnt)]))
+				       [(Integer/parseInt xi) (Double/parseDouble cnt)]))
 			       (vec))]))
