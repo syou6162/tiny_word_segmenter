b86da3991e661a6252cb3847e1c799aaedb6256a
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-20 22:09:48 +0900

    F値を評価する用のコードを追加

--- src/TinyWordSegmenter/util.clj
+++ src/TinyWordSegmenter/util.clj
@@ -12,3 +12,13 @@
 		  [xi (Double/parseDouble cnt)]))
 	  (vec))]))
 
+(defn get-f-value [gold prediction]
+  (let [freq (frequencies (map vector gold prediction))
+        tp (get freq [1 1] 0)
+        tn (get freq [-1 -1] 0)
+        fp (get freq [-1 1] 0)
+        fn (get freq [1 -1] 0)
+        recall (/ tp (+ tp fn))
+        precision (/ tp (+ tp fp))]
+    (/ (* 2.0 recall precision)
+       (+ recall precision))))
