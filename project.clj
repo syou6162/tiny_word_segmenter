d03f6708d9557b26094aa6c343dd0ce92efc59c0
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-20 22:15:23 +0900

    メモリを食うのでヒープの容量を増加させた

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.commons/commons-math "2.2"]]
-  :dev-dependencies [[swank-clojure "1.3.2"]])
+  :dev-dependencies [[swank-clojure "1.3.2"]]
+  :jvm-opts ["-Xmx3g" "-server" "-Dfile.encoding=UTF-8"])
