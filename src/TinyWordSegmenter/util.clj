526d3a147a98f291119d8a2feeabbcf302973449
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-20 22:08:46 +0900

    ラベルが0と1ではなく-1と+1に対応するように変更
    JavaのInteger/parseIntは"+1"に対応していないので若干汚ない...

--- src/TinyWordSegmenter/util.clj
+++ src/TinyWordSegmenter/util.clj
@@ -2,11 +2,13 @@
   (:use [clojure.contrib.string :only (split)]))
 
 (defn parse-line [line]
-  (let [[y fv] (-> (re-seq #"(-?1)\s(.*)" line)
+  (let [[y fv] (-> (re-seq #"([-+]?1)\s(.*)" line)
 		   (first)
 		   (rest))]
-    [(Integer/parseInt y) (->> fv
-			       (split #"\s")
-			       (map #(let [[xi cnt] (split #":" %)]
-				       [(Integer/parseInt xi) (Double/parseDouble cnt)]))
-			       (vec))]))
+    [(if (= y "+1") 1 -1)
+     (->> fv
+	  (split #"\s")
+	  (map #(let [[xi cnt] (split #":" %)]
+		  [xi (Double/parseDouble cnt)]))
+	  (vec))]))
+
