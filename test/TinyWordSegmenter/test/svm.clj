a13377b89767488d4258cf2fd623c16c96645ce5
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-20 22:14:38 +0900

    テストケースを追加

--- test/TinyWordSegmenter/test/svm.clj
+++ test/TinyWordSegmenter/test/svm.clj
@@ -5,17 +5,20 @@
 (deftest test-margin
   (let [weight {0 0.1, 1 0.2, 2 0.5, 3 -0.1}
 	fv [[1 1] [2 1]]]
-    (margin weight fv 1)))
+    (is (= (margin weight fv 1)
+	   0.7))
+    (is (= (margin weight fv -1)
+	   -0.7))))
 
-(deftest test-update
-  (let [examples [[[[1 1] [2 1]] 1]
-		  [[[1 2] [2 0]] -1]]
+(deftest test-update-weight
+  (let [examples [[1 [[1 1] [2 1]]]
+		  [-1 [[1 2] [2 0]]]]
 	init-weight {0 0.1, 1 0.2, 2 0.5, 3 -0.1}
-	max-iter 10
+	iter 10
 	eta 0.1
 	lambda 0.1]
-    (update examples init-weight max-iter eta lambda)))
+    (update-weight examples init-weight iter eta lambda)))
 
 (deftest test-classify
   (let [weight {0 0.1, 1 0.2, 2 0.5, 3 -0.1}]
-    (classify weight [[1 1] [2 1]] 1)))
+    (is (classify weight [[1 1] [2 1]] 1))))
