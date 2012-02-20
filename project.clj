5d8a39a0fe70ba18bda2c40344179cc26166b8cc
Yasuhisa Yoshida <syou6162@gmail.com>
2012-02-20 22:47:06 +0900

    versionを0.0.2に上げる

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject TinyWordSegmenter "0.0.1"
+(defproject TinyWordSegmenter "0.0.2"
   :description "Tiny word segmenter written in clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]
